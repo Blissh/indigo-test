2daa8c2060116d663b899b3388f0c1caf0eb6218
blissh <anthony.aka21@gmail.com>
2025-11-08 10:07:18 -0500

    Add initial project structure and configuration for ProductsAPI
    - Created .gitignore to exclude unnecessary files and directories.
    - Added appsettings.json and appsettings.Development.json for configuration.
    - Initialized ProductsAPI.csproj with .NET 9.0 framework and OpenAPI support.
    - Created Program.cs to set up the web application and configure services.
    - Added launchSettings.json for local development settings.
    - Defined domain entities: Product, Sale, SaleItems, and User.
    - Included ProductsAPI.http for API testing with sample requests.

---
+++ ProductsAPI/src/Domain/Entities/Sale.cs
@@ -0,0 +1,12 @@
+﻿namespace ProductsAPI.src.Domain.Entities
+{
+    public class Sale
+    {
+        private readonly List<SaleItems> _items = new List<SaleItems>();
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Total { get; set; }
+        public IReadOnlyCollection<SaleItems> Items => _items;
+        
+    }
+}
---
+++ ProductsAPI/src/Domain/Entities/SaleItems.cs
@@ -0,0 +1,14 @@
+﻿namespace ProductsAPI.src.Domain.Entities
+{
+    public class SaleItems
+    {
+        public int Id { get; set; }
+        public int SaleId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal Total { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
